e4adeaac7b31c56842e6e30e840a9526c2bd6525
Hansel He <hansel.he@intel.com>
2014-05-01 22:52:04 +0800

    change install directory to _install

---
+++ jni/LibVNCServer-0.9.9/myconfigure.sh
@@ -0,0 +1,1 @@
+../ndk_configure.sh --with-jpeg=$PWD/../libjpeg-turbo/libjpeg-turbo-1.3.1/_install
--- jni/ndk_configure.sh
+++ jni/ndk_configure.sh
@@ -3,5 +3,6 @@
 export SYSROOT=$NDK_HOME/platforms/android-19/arch-x86/  
 export CROSS_COMPILE=$NDK_HOME/toolchains/x86-4.8/prebuilt/linux-x86_64/bin/i686-linux-android-
 export CC=${CROSS_COMPILE}gcc
-mkdir -p $PWD/jni/dist
-./configure --prefix=$PWD/jni/dist --host=i686 CC="$CC --sysroot=$SYSROOT" $@
+INSTALL_DIR=$PWD/_install
+mkdir -p $INSTALL_DIR
+./configure --prefix=$INSTALL_DIR  --host=i686 CC="$CC --sysroot=$SYSROOT" $@
