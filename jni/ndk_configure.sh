09627360ad296dba6f5470aed0f44d7c968c4618
Hansel He <hansel.he@intel.com>
2014-05-01 21:45:49 +0800

    rename build.sh

--- jni/ndk_configure.sh
+++ jni/ndk_configure.sh
@@ -4,4 +4,4 @@ export SYSROOT=$NDK_HOME/platforms/android-19/arch-x86/
 export CROSS_COMPILE=$NDK_HOME/toolchains/x86-4.8/prebuilt/linux-x86_64/bin/i686-linux-android-
 export CC=${CROSS_COMPILE}gcc
 mkdir -p $PWD/jni/dist
-./configure --prefix=$PWD/jni/dist --host=i686 CC="$CC --sysroot=$SYSROOT"
+./configure --prefix=$PWD/jni/dist --host=i686 CC="$CC --sysroot=$SYSROOT" $@
